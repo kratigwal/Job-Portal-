9d56df74c0880af44b52c99b6432901917358964
kratigwal <krati.gwal@gmail.com>
2026-05-19 00:15:32 +0530

    Fixed applied jobs data mapping issue

--- frontend/src/hooks/useGetAppliedJobs.jsx
+++ frontend/src/hooks/useGetAppliedJobs.jsx
@@ -1,25 +1,59 @@
+// import { setAllAppliedJobs } from "@/redux/jobSlice";
+// import { APPLICATION_API_END_POINT } from "@/utils/constant";
+// import axios from "axios"
+// import { useEffect } from "react"
+// import { useDispatch } from "react-redux"
+
+// const useGetAppliedJobs = () => {
+//     const dispatch = useDispatch();
+
+//     useEffect(()=>{
+//         const fetchAppliedJobs = async () => {
+//             try {
+//                 const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, {withCredentials:true});
+//                 console.log(res.data);
+//                 if(res.data.success){
+//                     dispatch(setAllAppliedJobs(res.data.application));
+//                 }
+//             } catch (error) {
+//                 console.log(error);
+//             }
+//         }
+//         fetchAppliedJobs();
+//     },[])
+// };
+// export default useGetAppliedJobs;
+
 import { setAllAppliedJobs } from "@/redux/jobSlice";
 import { APPLICATION_API_END_POINT } from "@/utils/constant";
-import axios from "axios"
-import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const useGetAppliedJobs = () => {
     const dispatch = useDispatch();
 
-    useEffect(()=>{
+    useEffect(() => {
         const fetchAppliedJobs = async () => {
             try {
-                const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, {withCredentials:true});
-                console.log(res.data);
-                if(res.data.success){
-                    dispatch(setAllAppliedJobs(res.data.application));
+                const res = await axios.get(
+                    `${APPLICATION_API_END_POINT}/get`,
+                    { withCredentials: true }
+                );
+
+                console.log("Applied Jobs API:", res.data);
+
+                if (res.data.success) {
+                    dispatch(setAllAppliedJobs(res.data.appliedJobs)); 
                 }
+
             } catch (error) {
                 console.log(error);
             }
-        }
+        };
+
         fetchAppliedJobs();
-    },[])
+    }, []);
 };
+
 export default useGetAppliedJobs;
