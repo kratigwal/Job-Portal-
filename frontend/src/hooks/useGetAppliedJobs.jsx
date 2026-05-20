0001d74516f6bc197d62146e6bce9558aa8cdad0
kratigwal <krati.gwal@gmail.com>
2026-05-20 15:30:04 +0530

    Fixed applied jobs fetch issue

--- frontend/src/hooks/useGetAppliedJobs.jsx
+++ frontend/src/hooks/useGetAppliedJobs.jsx
@@ -1,27 +1,61 @@
+// // import { setAllAppliedJobs } from "@/redux/jobSlice";
+// // import { APPLICATION_API_END_POINT } from "@/utils/constant";
+// // import axios from "axios"
+// // import { useEffect } from "react"
+// // import { useDispatch } from "react-redux"
+
+// // const useGetAppliedJobs = () => {
+// //     const dispatch = useDispatch();
+
+// //     useEffect(()=>{
+// //         const fetchAppliedJobs = async () => {
+// //             try {
+// //                 const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, {withCredentials:true});
+// //                 console.log(res.data);
+// //                 if(res.data.success){
+// //                     dispatch(setAllAppliedJobs(res.data.application));
+// //                 }
+// //             } catch (error) {
+// //                 console.log(error);
+// //             }
+// //         }
+// //         fetchAppliedJobs();
+// //     },[])
+// // };
+// // export default useGetAppliedJobs;
+
 // import { setAllAppliedJobs } from "@/redux/jobSlice";
 // import { APPLICATION_API_END_POINT } from "@/utils/constant";
-// import axios from "axios"
-// import { useEffect } from "react"
-// import { useDispatch } from "react-redux"
+// import axios from "axios";
+// import { useEffect } from "react";
+// import { useDispatch } from "react-redux";
 
 // const useGetAppliedJobs = () => {
 //     const dispatch = useDispatch();
 
-//     useEffect(()=>{
+//     useEffect(() => {
 //         const fetchAppliedJobs = async () => {
 //             try {
-//                 const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, {withCredentials:true});
-//                 console.log(res.data);
-//                 if(res.data.success){
-//                     dispatch(setAllAppliedJobs(res.data.application));
+//                 const res = await axios.get(
+//                     `${APPLICATION_API_END_POINT}/get`,
+//                     { withCredentials: true }
+//                 );
+
+//                 console.log("Applied Jobs API:", res.data);
+
+//                 if (res.data.success) {
+//                     dispatch(setAllAppliedJobs(res.data.appliedJobs)); 
 //                 }
+
 //             } catch (error) {
 //                 console.log(error);
 //             }
-//         }
+//         };
+
 //         fetchAppliedJobs();
-//     },[])
+//     }, []);
 // };
+
 // export default useGetAppliedJobs;
 
 import { setAllAppliedJobs } from "@/redux/jobSlice";
@@ -31,29 +65,44 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 const useGetAppliedJobs = () => {
+
     const dispatch = useDispatch();
 
     useEffect(() => {
+
         const fetchAppliedJobs = async () => {
+
             try {
+
                 const res = await axios.get(
                     `${APPLICATION_API_END_POINT}/get`,
-                    { withCredentials: true }
+                    {
+                        withCredentials: true
+                    }
                 );
 
                 console.log("Applied Jobs API:", res.data);
 
                 if (res.data.success) {
-                    dispatch(setAllAppliedJobs(res.data.appliedJobs)); 
+
+                    dispatch(
+                        setAllAppliedJobs(
+                            res.data.application || 
+                            res.data.applications || 
+                            []
+                        )
+                    );
                 }
 
             } catch (error) {
+
                 console.log(error);
             }
         };
 
         fetchAppliedJobs();
-    }, []);
+
+    }, [dispatch]);
 };
 
 export default useGetAppliedJobs;
